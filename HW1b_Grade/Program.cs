96dbf3cffac6be4c2f097a3f3b9f80a4946e8141
SarahCardillo <sarah.cardillo@ou.edu>
2025-09-10 15:48:24 -0500

    done.

--- HW1b_Grade/Program.cs
+++ HW1b_Grade/Program.cs
@@ -15,6 +15,37 @@ namespace HW1b_Grade
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your first name?");
+            string FirstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            string LastName = Console.ReadLine();
+
+            Console.WriteLine("What is your student id?");
+            string StudentID = Console.ReadLine();
+
+            Console.WriteLine("What is your overall percentage grade for homeworks?");
+            double Homework = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for participations?");
+            double Participation = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for quizzes?");
+            double Quizzes = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for the midterm?");
+            double Midterm = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for the final?");
+            double Final = Convert.ToDouble(Console.ReadLine());
+
+            double finalGrade = (Homework * 0.20) + (Participation * 0.05) + (Quizzes * 0.20) + (Midterm * 0.25) + (Final * 0.30);
+
+            Console.WriteLine(FirstName + " " + LastName + "(" + StudentID + "), your final grade is " + finalGrade);
+
+
+
+            Console.ReadKey();
         }
     }
 }
